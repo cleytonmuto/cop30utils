b509fe960f7a3f12423f91ec15fa50705d5eb55c
Cleyton Muto <cleytonmuto@gmail.com>
2025-10-17 09:08:30 -0300

    chore: improved visibility

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,19 @@ function App() {
         <>
             <nav className="navbar navbar-expand bg-dark navbar-dark mb-3 p-2 rounded-3">
                 <div className="container-fluid gap-2">
-                    <Link className="navbar-brand" to="/">COP30 Utils</Link>
+                    <Link className="navbar-brand" to="/">
+                        COP30 Utils
+                    </Link>
                     <div className="navbar-nav flex-row gap-2">
-                        <Link className="nav-link" to="/to-upper">TO UPPER</Link>
-                        <Link className="nav-link" to="/to-lower">to lower</Link>
-                        <Link className="nav-link" to="/clean-numbers">Limpar CPF</Link>
+                        <Link className="nav-link" to="/to-upper">
+                            TO UPPER
+                        </Link>
+                        <Link className="nav-link" to="/to-lower">
+                            to lower
+                        </Link>
+                        <Link className="nav-link" to="/clean-numbers">
+                            Limpar CPF
+                        </Link>
                     </div>
                 </div>
             </nav>
